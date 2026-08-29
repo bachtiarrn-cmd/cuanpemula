f66d6869adde3559d2dba0fcf861766deb66a2c0
bachtiarrn-cmd <bachtiarrn@gmail.com>
2026-08-29 14:08:45 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Monetized Blog - Earn Passive Income",
   description: "A blog designed for earning through Google AdSense and affiliate marketing",
+  verification: {
+  google: "h4ebcGIaVJju5BbULRK9NePTz_mcN3YjF5s_j0JaSec",
+},
 };
 
 export default function RootLayout({
